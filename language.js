2921b61d095ad28d2fda7d9a8fac6039eb91e39e
Xhriss Tech <167217688+Xhriss24@users.noreply.github.com>
2024-06-15 17:04:26 +0300

    Update language.js

--- language.js
+++ language.js
@@ -1,32 +1,3 @@
-/*
-
- █▀▀█ █░▒█ █▀▀▀ █▀▀▀ █▄░▒█       █▀▀▀ █░░░ ▀█▀ █▀▀▀█ █▀▀█
- █░▒█ █░▒█ █▀▀▀ █▀▀▀ █▒█▒█       █▀▀▀ █░░░ ░█░ ▀▀▀▄▄ █▄▄█
- ▀▀█▄ ▀▄▄▀ █▄▄▄ █▄▄▄ █░░▀█       █▄▄▄ █▄▄█ ▄█▄ █▄▄▄█ █░▒█
-
-
-
-WHATSAPP BOT BY MR NIMA ( DARK MAKER OFC )
-
-WHATSAPP - 94719574492
-SUPPORT GROUP - https://t.me/queenelisasupport
-YOUTUBE - http://youtube.com/MRNIMAOFC
-
-
-ℹ️ SPECIAL INFORMATION ℹ️
-
-👀 There is Nothing To Change Anything 
-Queen Elisa Now Stable Version is 3.8.5 
-Do you have Any Problem About Bot Join Supoort Group
-
-Thanks For All Helpers
-@Thashi - For Voice
-@Thinura - For Helps
-@RaviduManoj - For Helps 
-@slrealtech - For Helps 
-
-@adiwajshing - For Web Api
-*/
 
 
 
